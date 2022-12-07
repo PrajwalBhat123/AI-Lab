0b05cf03ec1fbc7fbea69f6aac6c747427b36543
PrajwalBhat123 <75970070+PrajwalBhat123@users.noreply.github.com>
2022-12-07 09:26:17 +0530

    Add files via upload

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,211 +1,135 @@
-# Python3 program to print the path from root
-# node to destination node for N*N-1 puzzle
-# algorithm using Branch and Bound
-# The solution assumes that instance of
-# puzzle is solvable
+import numpy as np
+import sys
 
-# Importing copy for deepcopy function
-import copy
-
-# Importing the heap functions from python
-# library for Priority Queue
-from heapq import heappush, heappop
-
-# This variable can be changed to change
-# the program from 8 puzzle(n=3) to 15
-# puzzle(n=4) to 24 puzzle(n=5)...
-n = 3
-
-# bottom, left, top, right
-row = [1, 0, -1, 0]
-col = [0, -1, 0, 1]
-
-
-# A class for Priority Queue
-class priorityQueue:
-
-    # Constructor to initialize a
-    # Priority Queue
-    def __init__(self):
-        self.heap = []
-
-    # Inserts a new key 'k'
-    def push(self, k):
-        heappush(self.heap, k)
-
-    # Method to remove minimum element
-    # from Priority Queue
-    def pop(self):
-        return heappop(self.heap)
-
-    # Method to know if the Queue is empty
-    def empty(self):
-        if not self.heap:
-            return True
-        else:
-            return False
-
-
-# Node structure
-class node:
-
-    def __init__(self, parent, mat, empty_tile_pos,
-                 cost, level):
-        # Stores the parent node of the
-        # current node helps in tracing
-        # path when the answer is found
+class Node:
+    def __init__(self, state, parent, action):
+        self.state = state
         self.parent = parent
-
-        # Stores the matrix
-        self.mat = mat
-
-        # Stores the position at which the
-        # empty space tile exists in the matrix
-        self.empty_tile_pos = empty_tile_pos
-
-        # Stores the number of misplaced tiles
-        self.cost = cost
-
-        # Stores the number of moves so far
-        self.level = level
-
-    # This method is defined so that the
-    # priority queue is formed based on
-    # the cost variable of the objects
-    def __lt__(self, nxt):
-        return self.cost < nxt.cost
+        self.action = action
 
 
-# Function to calculate the number of
-# misplaced tiles ie. number of non-blank
-# tiles not in their goal position
-def calculateCost(mat, final) -> int:
-    count = 0
-    for i in range(n):
-        for j in range(n):
-            if ((mat[i][j]) and
-                    (mat[i][j] != final[i][j])):
-                count += 1
+class StackFrontier:
+    def __init__(self):
+        self.frontier = []
 
-    return count
+    def add(self, node):
+        self.frontier.append(node)
 
+    def contains_state(self, state):
+        return any((node.state[0] == state[0]).all() for node in self.frontier)
 
-def newNode(mat, empty_tile_pos, new_empty_tile_pos,
-            level, parent, final) -> node:
-    # Copy data from parent matrix to current matrix
-    new_mat = copy.deepcopy(mat)
+    def empty(self):
+        return len(self.frontier) == 0
 
-    # Move tile by 1 position
-    x1 = empty_tile_pos[0]
-    y1 = empty_tile_pos[1]
-    x2 = new_empty_tile_pos[0]
-    y2 = new_empty_tile_pos[1]
-    new_mat[x1][y1], new_mat[x2][y2] = new_mat[x2][y2], new_mat[x1][y1]
-
-    # Set number of misplaced tiles
-    cost = calculateCost(new_mat, final)
-
-    new_node = node(parent, new_mat, new_empty_tile_pos,
-                    cost, level)
-    return new_node
+    def remove(self):
+        if self.empty():
+            raise Exception("Empty Frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
 
 
-# Function to print the N x N matrix
-def printMatrix(mat):
-    for i in range(n):
-        for j in range(n):
-            print("%d " % (mat[i][j]), end=" ")
-
-        print()
-
-
-# Function to check if (x, y) is a valid
-# matrix coordinate
-def isSafe(x, y):
-    return x >= 0 and x < n and y >= 0 and y < n
+class QueueFrontier(StackFrontier):
+    def remove(self):
+        if self.empty():
+            raise Exception("Empty Frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
 
 
-# Print path from root node to destination node
-def printPath(root):
-    if root == None:
-        return
+class Puzzle:
+    def __init__(self, start, startIndex, goal, goalIndex):
+        self.start = [start, startIndex]
+        self.goal = [goal, goalIndex]
+        self.solution = None
 
-    printPath(root.parent)
-    printMatrix(root.mat)
-    print()
+    def neighbors(self, state):
+        mat, (row, col) = state
+        results = []
+
+        if row > 0:
+            mat1 = np.copy(mat)
+            mat1[row][col] = mat1[row - 1][col]
+            mat1[row - 1][col] = 0
+            results.append(('up', [mat1, (row - 1, col)]))
+        if col > 0:
+            mat1 = np.copy(mat)
+            mat1[row][col] = mat1[row][col - 1]
+            mat1[row][col - 1] = 0
+            results.append(('left', [mat1, (row, col - 1)]))
+        if row < 2:
+            mat1 = np.copy(mat)
+            mat1[row][col] = mat1[row + 1][col]
+            mat1[row + 1][col] = 0
+            results.append(('down', [mat1, (row + 1, col)]))
+        if col < 2:
+            mat1 = np.copy(mat)
+            mat1[row][col] = mat1[row][col + 1]
+            mat1[row][col + 1] = 0
+            results.append(('right', [mat1, (row, col + 1)]))
+
+        return results
+
+    def print(self):
+        solution = self.solution if self.solution is not None else None
+        print("Start State:\n", self.start[0], "\n")
+        print("Goal State:\n", self.goal[0], "\n")
+        print("Solution:\n ")
+        for action, cell in zip(solution[0], solution[1]):
+            print("action: ", action, "\n", cell[0], "\n")
+        print("---Goal Matrix obtained---")
+
+    def does_not_contain_state(self, state):
+        for st in self.explored:
+            if (st[0] == state[0]).all():
+                return False
+        return True
+
+    def solve(self):
+        self.num_explored = 0
+
+        start = Node(state=self.start, parent=None, action=None)
+        frontier = QueueFrontier()
+        frontier.add(start)
+
+        self.explored = []
+
+        while True:
+            if frontier.empty():
+                raise Exception("No solution")
+
+            node = frontier.remove()
+            self.num_explored += 1
+
+            if (node.state[0] == self.goal[0]).all():
+                actions = []
+                cells = []
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+
+            self.explored.append(node.state)
+
+            for action, state in self.neighbors(node.state):
+                if not frontier.contains_state(state) and self.does_not_contain_state(state):
+                    child = Node(state=state, parent=node, action=action)
+                    frontier.add(child)
 
 
-# Function to solve N*N - 1 puzzle algorithm
-# using Branch and Bound. empty_tile_pos is
-# the blank tile position in the initial state.
-def solve(initial, empty_tile_pos, final):
-    # Create a priority queue to store live
-    # nodes of search tree
-    pq = priorityQueue()
+start = np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
+goal = np.array([[2, 8, 1], [0, 4, 3], [7, 6, 5]])
 
-    # Create the root node
-    cost = calculateCost(initial, final)
-    root = node(None, initial,
-                empty_tile_pos, cost, 0)
+startIndex = (1, 1)
+goalIndex = (1, 0)
 
-    # Add root to list of live nodes
-    pq.push(root)
-
-    # Finds a live node with least cost,
-    # add its children to list of live
-    # nodes and finally deletes it from
-    # the list.
-    while not pq.empty():
-
-        # Find a live node with least estimated
-        # cost and delete it form the list of
-        # live nodes
-        minimum = pq.pop()
-
-        # If minimum is the answer node
-        if minimum.cost == 0:
-            # Print the path from root to
-            # destination;
-            printPath(minimum)
-            return
-
-        # Generate all possible children
-        for i in range(4):
-            new_tile_pos = [
-                minimum.empty_tile_pos[0] + row[i],
-                minimum.empty_tile_pos[1] + col[i], ]
-
-            if isSafe(new_tile_pos[0], new_tile_pos[1]):
-                # Create a child node
-                child = newNode(minimum.mat,
-                                minimum.empty_tile_pos,
-                                new_tile_pos,
-                                minimum.level + 1,
-                                minimum, final, )
-
-                # Add child to list of live nodes
-                pq.push(child)
-
-
-# Driver Code
-
-# Initial configuration
-# Value 0 is used for empty space
-initial = [[1, 2, 3],
-           [5, 6, 0],
-           [7, 8, 4]]
-
-# Solvable Final configuration
-# Value 0 is used for empty space
-final = [[1, 2, 3],
-         [4, 5, 6],
-         [7, 8, 0]]
-
-# Blank tile coordinates in
-# initial configuration
-empty_tile_pos = [1, 2]
-
-# Function call to solve the puzzle
-solve(initial, empty_tile_pos, final)
-
-# This code is contributed by Kevin Joshi
+p = Puzzle(start, startIndex, goal, goalIndex)
+p.solve()
+p.print()
